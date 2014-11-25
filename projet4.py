e1feb16684affd8b0a42f14b6c89dbb863b794d7
Zirani Jean-Luc <jlzirani@gmail.com>
2014-11-25 17:25:46 +0100

    one-liner 318 caractères

--- projet4.py
+++ projet4.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
+from sys import argv as a
 
-
-encode = lambda *s:[[(x&254)+(y&1)for x,y in w]for w in map(zip,*s)]  
-decode = lambda s:[[x&1 for x in y]for y in s]
-
+open(a[-1],'w').write("P2\n"+"\n".join(([lambda j,s,d:s[0:3]+[j([str((int(x)&254)+int(y)%2)for x,y in zip(s[3:],d[3:])])],lambda j,s:s[0:2]+['1', j([str(int(x)%2) for x in s[3:]])]]['de'in a[1]])(" ".join,*[open(x).read().split()[1:]for x in a[2:-1]])))if 3<len(a)<6 and a[1]in["codage","decodage"]else"bad parameters"
 
