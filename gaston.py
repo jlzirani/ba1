529c1d4746d690138e40e4824a21f1548fb8b279
Romain Fontaine <romain-fontaine@outlook.com>
2015-09-29 18:43:08 +0200

    one line missing loop

--- gaston.py
+++ gaston.py
@@ -1,42 +1,39 @@
-from random import seed, randint
+r,p,t,w,_ = (lambda r,p,t,w,_: (r,p,t,w, p("{}\tGaston arrive au bureau.", t))) (__import__("random").randint,lambda s,t:print(s.format("{:02}:{:02}".format(9+t//60,t%60))),0,0,__import__("random").seed(int("0"+"".join(filter(str.isdigit,input("Entrez une seed: "))))))
 
-def pretty(string, time):
-    print(string.format("{:02}:{:02}".format(9 + time//60, time%60)))
-
-seed(int("0" + "".join(filter(str.isdigit, input("Entrez une seed: ")))))
-time = 0
-worktime = 0
-
-pretty("{}\tGaston arrive au bureau.", time)
-while time < 540:
-    pretty("{}\tOK, pause !",time)
-    time += 50
-    if not randint(-1,1):
-        x = time + randint(-50,0)
-        pretty("{:02}:{:02}\tAttention, Prunelle arrive à {{}}".format(9+x//60, x%60), x+60)
-        c = (x+59-time)//10
+def l(t,w):
+    p("{}\tOK, pause !",t)
+    t += 50
+    if not r(-1,1):
+        x = t + r(-50,0)
+        p("{:02}:{:02}\tAttention, Prunelle arrive à {{}}".format(9+x//60, x%60), x+60)
+        c = (x+59-t)//10
 
         if c < 5:
             while (c>1):
-                pretty("{}\tC'est bon, encore le temps de faire une sieste. Zzz", time)
-                time += 20
-                if time >= 540:
-                    break
+                p("{}\tC'est bon, encore le temps de faire une sieste. Zzz", t)
+                t += 20
+                if t >= 540:
+                    return 540,w
                 c -= 2
         else:
-            pretty("{}\tOK, pause !", time)
-            time += 50
-            if time >= 540:
-                break
+            p("{}\tOK, pause !", t)
+            t += 50
+            if t >= 540:
+                return 540,w
 
-        worktime += x+150-time
-        pretty("{}\tIl faut travailler. M'enfin.", time)
-        time = x + 150
-        if time >= 540:
-            worktime -= time - 540
-            break
-        pretty("{}\tPrunelle est parti. \\O/", time)
+        w += x+150-t
+        p("{}\tIl faut travailler. M'enfin.", t)
+        t = x + 150
+        if t >= 540:
+            w -= t - 540
+            return 540,w
+        p("{}\tPrunelle est parti. \\O/", t)
+    if t < 540:
+        return l(t,w)
+    else:
+        return 540,w
 
-pretty("{}\tFin du service, dure journée", 540)
-print("Temps total travaillé: {} h {} min".format(worktime//60, worktime%60))
+l(t,w)
 
+
+(lambda w,t:(p("{}\tFin du service, dure journée", 540),print("Temps total travaillé: {} h {:02} min".format(w//60,w%60))))(w,t)
